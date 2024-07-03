4bc074e2c2eeba5cb38d619881d5e7285b73f4e0
Bunma93 <bunma93@gmail.com>
2024-07-03 11:45:59 +0700

    Js-obj7

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // write code here
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+
+let total = (obj) =>{
+    let total = 0;
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            let sum = Number(obj[key]);
+            total += sum;
+        }
+    } 
+    console.log(total);
+}
+
+total(salaries);
